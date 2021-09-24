04b1614ccb5bdae7f61a7ec66421d1e0a276dee6
carlosgomes1 <carlos_gomes.s@hotmail.com>
2021-09-24 20:16:08 -0300

    tela de login

--- iclean-frontend/src/pages/Login/index.tsx
+++ iclean-frontend/src/pages/Login/index.tsx
@@ -1,10 +1,51 @@
+import { FaFacebook, FaApple, FaGoogle } from 'react-icons/fa';
+
 import { Header } from '../../components/Header';
+
+import loginPNG from '../../assets/login.png';
+
+import { Container, Content, InputGroup } from './styles';
 
 export function Login() {
     return (
-        <>
+        <Container>
             <Header />
-            <h1>Login</h1>
-        </>
-    ); 
+            <Content>
+              <form>
+                <h1>Bem-vindo!</h1>
+                <span>Ainda não tem conta, <a>cadastre-se</a></span>
+
+                <InputGroup>
+                  <label htmlFor="">E-mail</label>
+                  <input type="text" />
+                </InputGroup>
+
+
+                <InputGroup>
+                  <label htmlFor="">Senha</label>
+                  <input type="text" />
+                </InputGroup>
+
+                <strong>Esqueceu a senha?</strong>
+
+                <button type="submit">Login</button>
+
+                <span>ou continuar com</span>
+
+                <div className="buttons">
+                  <button>
+                    <FaGoogle size={36} color="#F14336" />
+                  </button>
+                  <button>
+                    <FaFacebook size={36} color="#5762C3" />
+                  </button>
+                  <button>
+                    <FaApple size={36} color="#444B59" />
+                  </button>
+                </div>
+              </form>
+              <img src={loginPNG} alt="Login IMG" />
+            </Content>
+        </Container>
+    );
 };
--- iclean-frontend/src/styles/global.ts
+++ iclean-frontend/src/styles/global.ts
@@ -12,6 +12,7 @@ export const GlobalStyle = createGlobalStyle`
         margin: 0;
         padding: 0;
         box-sizing: border-box;
+        outline: 0;
     }
 
     html {
