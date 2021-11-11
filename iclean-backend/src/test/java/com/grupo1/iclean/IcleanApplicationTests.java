5bba876a692e4ff3706450a8d9413e6db4b9ad35
Roberto Gomes <roberto.gsl.2002@gmail.com>
2021-11-11 18:20:04 -0300

    banco de dados REALMENTE integrado chupa CARLINHOS

---
+++ iclean-backend/src/test/java/com/grupo1/iclean/IcleanApplicationTests.java
@@ -0,0 +1,13 @@
+package com.grupo1.iclean;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IcleanApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
