e85231c84530d671b0af4d504fdaaf5d1846e7ff
Guilherme Soares De Castro <guilherme.castro@linx3.com>
2021-11-18 21:24:25 -0300

    correction

--- iclean-frontend/src/components/PageServices/index.tsx
+++ iclean-frontend/src/components/PageServices/index.tsx
@@ -1,12 +1,20 @@
+import { Container, Content, Space } from "./styles";
+
 import { OpenServices } from "../OpenServices";
 import { RecentProfessional } from "../RecentProfessional";
-
+import { Header } from "../Header";
+import { BackButton } from "../BackButton";
 
 export function PageServices() {
   return (
-    <>
-    <OpenServices />
-    <RecentProfessional />
-    </>
+    <Container>
+      <Header />
+      <BackButton />
+      <Content>
+        <OpenServices />
+        <Space />
+        <RecentProfessional />
+      </Content>
+    </Container>
   );
 }
--- iclean-frontend/src/components/PageServices/styles.ts
+++ iclean-frontend/src/components/PageServices/styles.ts
@@ -2,44 +2,25 @@ import styled from "styled-components";
 
 export const Container = styled.div`
   width: 100%;
-  height: 100%;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+`;
+
+export const Content = styled.div`
+  display: flex;
+  flex-direction: row;
+  width: 80%;
+  padding-top: 1.5rem;
 `;
 
 export const Title = styled.div`
   color: white;
   font-size: 2rem;
   margin-bottom: 1rem;
-`;
+  `;
 
-export const Content = styled.div``;
-
-export const CardInvite = styled.div`
-  display: flex;
-  align-items: center;
-  justify-content: center;
-  margin-bottom: 1rem;
-  width: 100%;
-  min-height: 4.5rem;
-  background-color: white;
-  border-radius: 4px;
-
-  div{
-      width: 23%;
-  }
-
-  img {
-      max-width: 4.5rem;
-      padding: 0rem 1rem;
-      margin-left: 1rem;
-  }
-
-  span {
-    display: flex;
-    justify-content: space-between;
-    width: 100%;
-    text-align: center;
-    vertical-align: middle;
-    padding: 0rem 1rem;
-    font-size: 1.3rem;
-  }
-`;
+export const Space = styled.div`
+  width: 10%;
+  `;
