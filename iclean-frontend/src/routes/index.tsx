c40a6d7c57d8c203a6324941230ac5f11400ed16
Gs-soares <guilherme.castro@linx3.com>
2021-11-21 20:02:06 -0300

    att

--- iclean-frontend/src/routes/index.tsx
+++ iclean-frontend/src/routes/index.tsx
@@ -15,6 +15,7 @@ import { InviteForService } from '../components/InviteForService';
 import { OpenServices } from '../components/OpenServices';
 import { ConfirmInvite } from '../components/ConfirmInvite';
 import { PageServices } from '../components/PageServices';
+import { CardServices } from '../components/CardServices';
 
 export function Routes() {
     return (
@@ -27,7 +28,7 @@ export function Routes() {
             <Route path="/listOnMap" isPrivate component={ListOnMap} />
             <Route path="/profile" isPrivate component={Profile} />
             <Route path="/servicesHired" isPrivate component={ServicesHired} />
-            {/* <Route path="/testeComponente" component={Candidature} /> */}
+            <Route path="/testeComponente" component={CardServices} />
             {/* <Route path="/newService" component={NewService} />
             <Route path="/dashboard" component={Dashboard} />
             <Route path="/listOnMap" component={ListOnMap} />
