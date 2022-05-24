1f72420c4986ccbbf2ba92e94b7dd589ae669c25
carlosgomes1 <carlos_gomes.s@hotmail.com>
2022-05-23 23:31:00 -0300

    subindo mobile contratante

---
+++ IcleanMobile/app/src/main/java/com/example/icleanmobile/ContratanteSearch3.kt
@@ -0,0 +1,11 @@
+package com.example.icleanmobile
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class ContratanteSearch3 : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_contratante_search3)
+    }
+}
